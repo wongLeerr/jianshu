afc5e09113954b4480efc5b0548c83a5be8d3928
wongleer <770013479@qq.com>
2023-05-27 01:30:14 +0800

    项目初始化

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Globalstyle } from './style'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Globalstyle></Globalstyle>
+        hello,react
+      </div>
+    )
+  }
+}
